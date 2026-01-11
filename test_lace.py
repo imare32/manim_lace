49176156b3565778b4ae2e22dd58fcdfac96617b
imare32 <imare32@outlook.com>
2026-01-12 02:50:27 +0800

    Add Images

--- test_lace.py
+++ test_lace.py
@@ -5,21 +5,21 @@ from manim import *
 class TestLaceScene0(Scene):
     def construct(self):
         star = StarRosette(10, 2).scale(1)
-        lace = Lace(star, offset=0.1, stroke_color=BLACK, stroke_width=1, fill_color=RED, fill_opacity=0.5)
+        lace = Lace(star, offset=0.1, stroke_color=BLACK, stroke_width=1, fill_color=WHITE, fill_opacity=0.5)
         self.add(lace)
 
 
 class TestLaceScene1(Scene):
     def construct(self):
         star = StarRosette(10, 3).scale(1)
-        lace = Lace(star, offset=0.1, stroke_color=BLACK, stroke_width=1, fill_color=RED, fill_opacity=0.5)
+        lace = Lace(star, offset=0.1, stroke_color=BLACK, stroke_width=1, fill_color=WHITE, fill_opacity=0.5)
         self.add(lace)
 
 
 class TestLaceScene2(Scene):
     def construct(self):
         grid = NumberPlane().background_lines
-        lace = Lace(grid, offset=0.1, stroke_color=BLACK, stroke_width=1, fill_color=RED, fill_opacity=0.5)
+        lace = Lace(grid, offset=0.1, stroke_color=BLACK, stroke_width=1, fill_color=WHITE, fill_opacity=0.5)
         self.add(lace)
 
 
@@ -39,9 +39,9 @@ class TestLaceScene3(Scene):
 if __name__ == "__main__":
     with tempconfig({"quality": "low_quality", "preview": False, "verbosity": "WARNING", "background_color": WHITE}):
         TestLaceScene0().render()
-    with tempconfig({"quality": "low_quality", "preview": False, "verbosity": "WARNING", "background_color": WHITE}):
-        TestLaceScene1().render()
-    with tempconfig({"quality": "low_quality", "preview": False, "verbosity": "WARNING", "background_color": WHITE}):
-        TestLaceScene2().render()
-    with tempconfig({"quality": "low_quality", "preview": False, "verbosity": "WARNING", "background_color": WHITE}):
-        TestLaceScene3().render()
+    # with tempconfig({"quality": "low_quality", "preview": False, "verbosity": "WARNING", "background_color": WHITE}):
+    #     TestLaceScene1().render()
+    # with tempconfig({"quality": "low_quality", "preview": False, "verbosity": "WARNING", "background_color": WHITE}):
+    #     TestLaceScene2().render()
+    # with tempconfig({"quality": "low_quality", "preview": False, "verbosity": "WARNING", "background_color": WHITE}):
+    #     TestLaceScene3().render()
